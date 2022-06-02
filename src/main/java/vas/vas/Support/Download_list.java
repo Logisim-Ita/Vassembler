9c1121e7b328a56efcbd714c37e8527cc7688432
Andrei Ursu <87210146+Vaxick@users.noreply.github.com>
2022-06-02 02:13:39 +0200

    Updates

--- src/main/java/vas/vas/Support/Download_list.java
+++ src/main/java/vas/vas/Support/Download_list.java
@@ -4,10 +4,12 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Scanner;
 
 public class Download_list {
     public static String instructions;
-
+    //Function to read the instructions form github
+    /*
     public static void update_list() throws IOException {
         BufferedInputStream in = new BufferedInputStream(new URL("https://github.com/Logisim-Ita/Vassembler/releases/download/Instructions-latest/instruction.txt").openStream());
         byte[] data = new byte[1024];
@@ -17,6 +19,25 @@ public class Download_list {
             str = new String(data, 0, numRead);
         }
         instructions = str;
+        System.out.println(instructions);
+    }*/
+
+    public static void update_list(){
+        try {
+            URL url = new URL("https://github.com/Logisim-Ita/Vassembler/releases/download/Instructions-latest/instruction.txt");
+            Scanner s = new Scanner(url.openStream());
+            StringBuilder str = new StringBuilder();
+            while (s.hasNext()) {
+                str.append(s.nextLine()).append("\n");
+            }
+            instructions = str.toString();
+            System.out.println(instructions);
+
+        }
+        catch(IOException ex) {
+
+            ex.printStackTrace();
+        }
     }
 
     public static boolean internet_check(){
